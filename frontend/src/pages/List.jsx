874f6d58b317155bd8f052310abc2abf6e580638
Ameya Potdar <ameyapradippotdar@gmail.com>
2024-11-01 19:12:27 +0530

    added logout functionality to the code

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { useAuth } from '../AuthContext'
 
 const List = () => {
-  const {user} = useAuth();
+  const {user,logout} = useAuth();
   return (
     <div>
       {user && <h1>Welcome, {user}!</h1>}
+      <button onClick={logout}>Logout</button>
       Welcome to List!
     </div>
   )
